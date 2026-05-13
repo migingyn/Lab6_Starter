d6f20f059f837068b9c90a1f0ecbcfc69bcd4315
Mikey Nguyen <nmigi32@gmail.com>
2026-05-13 05:00:33 -0700

    clear button issue fixed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -95,15 +95,15 @@ function initFormHandler() {
 		const recipes = getRecipesFromStorage();
 		recipes.push(recipeObject);
 		saveRecipesToStorage(recipes);
+	});
 	// B10. TODO - Get a reference to the "Clear Local Storage" button
-		const clearButton = document.querySelector('.danger');
+	const clearButton = document.querySelector('.danger');
 	// B11. TODO - Add a click event listener to clear local storage button
-		clearButton.addEventListener('click', () => {
+	clearButton.addEventListener('click', () => {
 	// Steps B12 & B13 will occur inside the event listener from step B11
 	// B12. TODO - Clear the local storage
-			localStorage.removeItem('recipes');
+		localStorage.clear();
 	// B13. TODO - Delete the contents of <main>
-			main.innerHTML = '';
-		});
+		document.querySelector('main').innerHTML = '';
 	});
 }
